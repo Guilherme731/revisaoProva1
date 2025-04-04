e016685cc987238611da0574fb5f299ed405b920
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-04-04 08:34:10 -0300

    Exercicio 1 e HTML padrão

---
+++ script.js
@@ -0,0 +1,14 @@
+function ex1(){
+    let qtdNumeros = parseInt(prompt('Quantos números você deseja somar?'));
+    let soma = 0;
+    for(let i = 0; i < qtdNumeros; i++){
+        let numero = parseFloat(prompt('Digite o ' + (i + 1) + 'º numero'));
+        soma = soma + numero;
+    }
+
+    console.log('A soma dos números é igual a ' + soma)
+}
+
+function ex2(){
+    
+}
